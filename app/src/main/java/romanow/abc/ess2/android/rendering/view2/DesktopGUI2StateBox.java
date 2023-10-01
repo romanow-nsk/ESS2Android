430d59fe30c21cf93b002ea0e70f57dd37b6e1f7
romanow-nsk <romanow@ngs.ru>
2023-10-01 20:13:32 +0700

    Надпись-индекс

--- app/src/main/java/romanow/abc/ess2/android/rendering/view2/DesktopGUI2StateBox.java
+++ app/src/main/java/romanow/abc/ess2/android/rendering/view2/DesktopGUI2StateBox.java
@@ -55,6 +55,7 @@ public class DesktopGUI2StateBox extends View2BaseDesktop {
             case 0x00C0C0C0: return R.drawable.balllightgray;
             case 0x00FF0000: return R.drawable.ballred;
             case 0x0000FF00: return R.drawable.ballgreen;
+            case 0x0000A000: return R.drawable.balldarkgreen;
             case 0x00FFFF00: return R.drawable.ballyellow;
             case 0x00F0A000: return R.drawable.balldarkyellow;
             }
